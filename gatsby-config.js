6a2650885bab03cb00688209254e77cdc91e6f36
genius-softdev <webdevelopercrane@gmail.com>
2020-03-14 08:42:42 +0700

    Implement and Use Graphql query on header.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,10 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: 'Full-Stack Bootcamp', 
+    author: 'Phantom'
+  }, 
   plugins: [
     'gatsby-plugin-sass'
   ]
