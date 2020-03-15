c576d9a6f81b72ee3f4611922ba17a8b753e27b6
genius-softdev <webdevelopercrane@gmail.com>
2020-03-16 01:48:01 +0700

    Implement 404 page and Head of each page using Helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
+import Head from '../components/head'
 
 const IndexPage = () => {
   return (
     <Layout>
+      <Head title="Home" />
       <h1>Hello.</h1>
       <h2>I'm Phantom, a full-stack developer living in beautiful City.</h2>
       <p>
